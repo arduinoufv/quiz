f601de2adfd913423c89d51f7f52a181be76e4ce
Ricardo Ferreira <arduinoufv@gmail.com>
2023-01-17 10:28:13 -0300

    Update functions.py

--- quizlib/functions.py
+++ quizlib/functions.py
@@ -2,7 +2,6 @@ from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
 import random 
 
-
 def random_quiz_list(l,head=""):
   x = random.randrange(len(l))
   print(head+l[x][0]+"?")
@@ -14,3 +13,21 @@ def random_quiz_list(l,head=""):
       return "Try again !"  
 
   interact(op,answer="^")
+  
+def random_quiz_dropdown_list(l,head=""):
+  x = random.randrange(len(l))
+  print(head+l[x][0]+"?")
+ 
+  r = []
+  for i in l:
+    r.append(i[1])
+
+  def op(answer):
+    if (answer== l[x][1]):
+      return "Well Done !"
+    else: 
+      return "Try again ..."  
+  
+  interact(op,answer=r)
+
+  
